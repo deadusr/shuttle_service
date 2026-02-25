1d4dcba4675ae59bbe58d549e6796321cb2216b8
deadusr <iura228.lebedev8775@gmail.com>
2026-02-25 22:02:51 +0300

    Исправил ошибку - теперь можно назначить рейс только на дату рейса

--- src/routes/drivers/index.tsx
+++ src/routes/drivers/index.tsx
@@ -1,6 +1,6 @@
 import { createFileRoute, useSearch } from '@tanstack/react-router'
 import DriverScheduleTable from '../../components/schedule/DriverScheduleTable'
-import { format } from 'date-fns'
+import { format, startOfWeek } from 'date-fns'
 
 export const Route = createFileRoute('/drivers/')({
     component: RouteComponent,
@@ -9,11 +9,11 @@ export const Route = createFileRoute('/drivers/')({
 function RouteComponent() {
     const search = useSearch({ strict: false });
     const dateStr = (search.date as string) || format(new Date(), 'yyyy-MM-dd');
-    const date = new Date(dateStr);
+    const startDate = startOfWeek(new Date(dateStr), { weekStartsOn: 1 });
 
     return (
         <main className="flex-1 flex flex-col bg-gray-100 min-w-0 overflow-hidden relative">
-            <DriverScheduleTable startDate={date} />
+            <DriverScheduleTable startDate={startDate} />
         </main>
     )
 }
